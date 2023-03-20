206d4cffd63172e73857180c644fb3dd16f81e72
XueFeng <77104643+Lns-XueFeng@users.noreply.github.com>
2023-03-20 09:13:35 +0800

    Update response.py

--- vilivili/response.py
+++ vilivili/response.py
@@ -1,13 +1,23 @@
+"""
+响应报文包含状态行、报文首部、空行、报文主体四个部分
+在响应报文中，状态行和报文首部可以看成一个整体叫作响应头，报文主体又叫响应体
+
+因此最后在Response返回：响应头 + 空行 + 响应体
+完整的返回给浏览器：
+    b'HTTP/1.1 200 OK\r\n'              # 状态行
+    b'Content-Type: text/html\r\n\r\n'  # 响应头
+    b'<h1>Hello World</h1>'             # 响应体
+"""
+
+
 class Response(object):
     """响应类"""
 
-    # 根据状态码获取原因短语
     reason_phrase = {
         200: 'OK',
         405: 'METHOD NOT ALLOWED',
     }
-
-    # 默认响应首部字段，指定响应内容的类型为 HTML
+    
     _headers = {
         'Content-Type': 'text/html; charset=utf-8',
     }
