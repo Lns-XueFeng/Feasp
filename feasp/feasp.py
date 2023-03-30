b62d6b857822f5a51cac13731adcbdf7bb529427
XueFeng <77104643+Lns-XueFeng@users.noreply.github.com>
2023-03-30 12:03:46 +0800

    Update feasp.py

--- feasp/feasp.py
+++ feasp/feasp.py
@@ -1,19 +1,44 @@
 import os
 import threading
 
-from werkzeug.wrappers import Request, Response
+from werkzeug.wrappers import Request as BaseRequest, Response as BaseResponse
 
 
-http_404 = Response("<html><body>Not Found 404</body></html>", mimetype="text/html")
-http_404.status = "404 Not Found"
-http_405 = Response('<html><body>Method Not Allowed</body></html>', mimetype="text/html")
-http_405.status = "405 Method Not Allowed"
-http_500 = Response('<html><body>Server Internal Error</body></html>', mimetype="text/html")
-http_500.status = "500 Server Internal Error"
-
-
-global_var = {}
+global_var = {}   # 存一些需要全局使用的变量
 http_local = threading.local()
+
+
+def render_template(filename):
+    filepath = os.path.join(global_var["user_base_dir"], "template", filename)
+    with open(filepath, 'r') as fp:
+        content = fp.read()
+    return content
+
+
+def deal_favicon():
+    filepath = os.path.join(global_var["user_base_dir"], "static", "favicon.ico")
+    if os.path.exists(filepath):
+        with open(filepath, 'rb') as fp:
+            content = fp.read()
+        return content
+    return Error.http_404
+
+
+class Request(BaseRequest):
+    pass
+
+
+class Response(BaseResponse):
+    pass
+
+
+class Error:
+    http_404 = Response("<html><body>Not Found 404</body></html>", mimetype="text/html")
+    http_404.status = "404 Not Found"
+    http_405 = Response('<html><body>Method Not Allowed</body></html>', mimetype="text/html")
+    http_405.status = "405 Method Not Allowed"
+    http_500 = Response('<html><body>Server Internal Error</body></html>', mimetype="text/html")
+    http_500.status = "500 Server Internal Error"
 
 
 class Method:
@@ -23,33 +48,30 @@ class Method:
     PUT = "PUT"
 
 
-def render_template(filename):
-    filepath = os.path.join(global_var["base_dir"], "template", filename)
-    with open(filepath, 'r') as fp:
-        content = fp.read()
-    return content
-
-
-class Application:
+class Feasp:
 
     def __init__(self, filename):
-        self.url_func_map = {}
-        self.base_dir = os.path.abspath(os.path.dirname(filename))
-        global_var["base_dir"] = self.base_dir
+        # url与view_func的映射
+        self.url_func_map = {"/favicon.ico": (deal_favicon, ["GET"])}
+        # 用户的包文件路径
+        self.user_base_dir = os.path.abspath(os.path.dirname(filename))
+        global_var["user_base_dir"] = self.user_base_dir
 
     def dispatch(self, path, method):
+        """ 处理请求并返回对应视图函数的响应 """
         values = self.url_func_map.get(path, None)
         if values is None:
-            return http_404
+            return Error.http_404
 
         view_func, methods = values
         view_func_return = view_func()
         if method not in methods:
-            return http_405
+            return Error.http_405
 
-        mimetype = "text/html"
         if isinstance(view_func_return, str):
-            pass
+            mimetype = "text/html"
+        elif isinstance(view_func_return, bytes):
+            mimetype = "image/x-icon"
         elif isinstance(view_func_return, dict):
             temp = "{"
             for k, v in view_func_return.items():
@@ -62,11 +84,12 @@ class Application:
             view_func_return = "[" + temp + "]"
             mimetype = "text/plain"
         else:
-            return http_500
+            return Error.http_500
 
         return Response(view_func_return, mimetype=mimetype)
 
     def route(self, path, methods):
+        """ 将路径与视图函数进行绑定 """
         if methods is None:
             methods = [Method.GET]
 
@@ -87,3 +110,4 @@ class Application:
     def run(self):
         from werkzeug.serving import run_simple
         run_simple("127.0.0.1", 8000, self.wsgi_apl)
+
