95e796a78543d763d285d2107dd72c44315a6280
handandkeep <1477792904@qq.com>
2023-04-19 23:28:02 +0800

    update

--- feasp/__init__.py
+++ feasp/__init__.py
@@ -1,9 +1,10 @@
 from .feasp import Feasp
-from .feasp import render_template, url_for, redirect
+from .feasp import render_template, url_for, redirect, session
 
 __init__ = [
     Feasp,
     render_template,
     url_for,
-    redirect
+    redirect,
+    session,
 ]
