1079285b5bffbb65ec1c28036172b0e147105ab1
handandkeep <1477792904@qq.com>
2023-04-20 18:26:44 +0800

    支持POST请求, 增加form, 比如可以进行表单验证

--- feasp/feasp.py
+++ feasp/feasp.py
@@ -7,6 +7,7 @@ Create Time: 2023.03.27
 import os
 import json
 import threading
+from urllib.parse import parse_qs
 
 
 class NotFound(Exception):
@@ -97,6 +98,9 @@ def deal_images(image_path):
 def deal_static(link_path):
     """
       处理css, js文件的请求
+      link_path: static目录下的文件路径
+      比如example/static下的style.css
+      你应该在html中这样写：/style.css 或者 style.css
     """
     if link_path[0] == "/":
         link_path = link_path[1:]
@@ -120,7 +124,8 @@ class Request:
       self.method: http请求方法
       self.path: http请求路径(资源路径)
       self.args: url中的参数
-      self.cookie: 存储已解析浏览器的cookie """
+      self.cookie: 存储已解析浏览器的cookie
+      self.form: 如果有POST请求则进行存储 """
 
     def __init__(self, environ):
         self.protocol = environ.get('SERVER_PROTOCOL')
@@ -128,14 +133,25 @@ class Request:
         self.path = environ.get("PATH_INFO")
         self.qs = environ.get("QUERY_STRING")
         self.uri = environ.get("REQUEST_URI")
+        self.cookie = self.get_cookie(environ)
+        self.form = self.get_form(environ)
 
+    @staticmethod
+    def get_cookie(environ):
+        cookie = {}
         http_cookie = environ.get("HTTP_COOKIE")
-        self.cookie = dict()   # 存储浏览器返回的cookie
         if http_cookie is not None:
             cl = http_cookie.split(" ")
             for kv in cl:
                 k, v = tuple(kv.split("="))
-                self.cookie[k] = v
+                cookie[k] = v
+        return cookie
+
+    @staticmethod
+    def get_form(environ):
+        rb_size = int(environ.get('CONTENT_LENGTH', 0))
+        rb = environ["wsgi.input"].read(rb_size)
+        return parse_qs(rb)
 
     def __repr__(self):
         return f"<{type(self).__name__} {self.method} {self.protocol} {self.path}>"
@@ -209,7 +225,7 @@ class Error:
 class Feasp:
 
     """ Feasp: 一个简易的Web框架, 基于WSGI规范, 仅用来学习交流
-      实现了路由注册, WSGI Application, 请求的分发, 并保证多线程情况下的请求-响应的安全返回
+      实现了路由注册, WSGI Application, 请求的分发, 各种资源的自动处理
       使用示例:
       from feasp import Feasp
 
@@ -219,16 +235,25 @@ class Feasp:
       def index():
           return 'Hello Feasp !' """
 
+    # 指向Request类
+    request_class = Request
+
+    # 指向Response类
+    response_class = Response
+
     def __init__(self, filename):
         # url与view_func的映射
-        self.url_func_map = dict()
+        self.__url_func_map = {}
+
+        # self.__url_func_map 传入全局字典
+        _global_var["url_func_map"] = self.__url_func_map
+
         # 用户的包文件路径
-        self.user_pkg_abspath = os.path.abspath(os.path.dirname(filename))
-        _global_var["user_pkg_abspath"] = self.user_pkg_abspath
-        # self.url_func_map 传入全局字典
-        _global_var["url_func_map"] = self.url_func_map
-        # 待存储request中的cookie
-        self.cookie = dict()
+        self.__user_pkg_abspath = os.path.abspath(os.path.dirname(filename))
+        _global_var["user_pkg_abspath"] = self.__user_pkg_abspath
+
+        # 指向当前请求对象, 可使用户访问
+        self.request = None
 
     def dispatch(self, path, method):
         """ 处理请求并返回对应视图函数的响应 """
@@ -237,19 +262,19 @@ class Feasp:
             if bq in path:
                 mimetype = "image/x-icon"
                 content = deal_images(path)
-                return Response(content, mimetype=mimetype)
+                return self.response_class(content, mimetype=mimetype)
         # 处理css, js文件请求
         for bq in (".css", ".js"):
             if bq in path and bq == ".css":
                 mimetype = "text/css"
                 content = deal_static(path)
-                return Response(content, mimetype=mimetype)
+                return self.response_class(content, mimetype=mimetype)
             if bq in path and bq == ".js":
                 mimetype = "application/javascript"
                 content = deal_static(path)
-                return Response(content, mimetype=mimetype)
+                return self.response_class(content, mimetype=mimetype)
         # 处理视图函数相关请求
-        values = self.url_func_map.get(path, None)
+        values = self.__url_func_map.get(path, None)
         if values is None:
             return Error.http_404
 
@@ -271,7 +296,7 @@ class Feasp:
         else:
             return Error.http_500
 
-        return Response(view_func_return, mimetype=mimetype)
+        return self.response_class(view_func_return, mimetype=mimetype)
 
     def route(self, path, methods):
         """ 将路径与视图函数进行绑定 """
@@ -283,33 +308,34 @@ class Feasp:
                 return func(*args, **kwargs)
 
             endpoint = func.__name__   # 此处端点为视图函数的名称
-            self.url_func_map[path] = (endpoint, func, methods)
+            self.__url_func_map[path] = (endpoint, func, methods)
             return wrapper
         return decorator
 
     def wsgi_apl(self, environ, start_response):
-        _http_local.request = Request(environ)
+        _http_local.request = self.request_class(environ)
 
-        # 当并发请求时, 可能会造成cookie或session的错误竞争存取
-        # 因此使用锁将取cookie和返回响应(包括session相关操作)设置为原子性执行
+        # 当并发请求时, 可能会造成request或session的错误竞争存取
+        # 因此使用锁将取session和返回响应(包括session相关操作)设置为原子性执行
+        # 这样就可以确保并发时每一个请求都能返回正确的request以及响应给用户
         with threading.Lock():
-            self.cookie = _http_local.request.cookie   # 传递cookie
+            self.request = _http_local.request   # 传递request
             _http_local.response = self.dispatch(
                 _http_local.request.path, _http_local.request.method)
         return _http_local.response(environ, start_response)
 
-    def run(self, host, port, multithread=True):
-        if multithread:   # 默认使用支持多线程的run_simple
-            from werkzeug.serving import run_simple
-            run_simple(host, port, self.wsgi_apl)
-
-        if not multithread:   # 否则使用单线程的make_server
+    def run(self, host, port, multithread=False):
+        if not multithread:   # 默认make_server: 仅支持单线程
             from wsgiref.simple_server import make_server
             with make_server(host, port, self.wsgi_apl) as httpd:
                 print(f"* Running on http://{host}:{port}")
                 httpd.serve_forever()
 
+        if multithread:   # run_simple: 支持多线程
+            from werkzeug.serving import run_simple
+            run_simple(host, port, self.wsgi_apl)
 
-_global_var = dict()   # 存一些需要全局使用的变量
+
+_global_var = {}   # 存一些需要全局使用的变量
 _http_local = threading.local()   # 保证多线程请求时的线程安全
-session = dict()   # session会话: 用户用于让浏览器设置cookie
+session = {}   # session会话: 用户用于让浏览器设置并存储cookie在本地
