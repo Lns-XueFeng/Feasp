7b11d4e18c9c9b4ec631a58aa80aa160accaefec
handandkeep <1477792904@qq.com>
2023-04-21 22:55:24 +0800

    add show_variable

--- example/app.py
+++ example/app.py
@@ -1,6 +1,11 @@
 from feasp import Feasp
 from feasp import render_template, url_for, redirect, session
 
+"""
+用来验证Feasp实现的功能
+一般是实现一个功能, 这里就编写一个视图函数来进行验证功能
+或者是先编写一个视图函数, 然后去框架中实现支持
+"""
 
 app = Feasp(__name__)
 
@@ -38,8 +43,8 @@ def redirect_():
 @app.route("/set_cookies", methods="GET")
 def set_cookies():
     # 通过session来让浏览器设置cookie
-    session["name"] = "xue"
-    session["ppp"] = "ppp"
+    session["name"] = "xuefeng"
+    session["hobby"] = "code"
     return "Set Cookies"
 
 
@@ -59,11 +64,20 @@ def login():
     if app.request.method == "POST":
         if app.request.form["username"] == "xuefeng" \
                 and app.request.form["password"] == "123456789":
+            # session只能设置: 让浏览器设置cookie并存储
+            # 如果需要读取cookie, 可以这样: app.request.cookie
             session["username"] = "xuefeng"
             session["password"] = "123456789"
             # 至此可以考虑实现@login_required装饰器了
             return "Your login correctly !"
     return render_template("login.html")
+
+
+@app.route("/variable/<variable:value>", methods=["GET"])
+def show_variable(name):
+    """ 传入的变量必须与在模板中的一致
+      且只能以key=value形式传入所需渲染的变量与值"""
+    return render_template("variable.html", name=name, love="you")
 
 
 if __name__ == "__main__":
