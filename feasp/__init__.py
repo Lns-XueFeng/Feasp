e89957acfc4058016116670a19c605b390658272
handandkeep <1477792904@qq.com>
2023-04-25 23:01:54 +0800

    update

--- feasp/__init__.py
+++ feasp/__init__.py
@@ -1,10 +1,9 @@
-from .feasp import Feasp, Response
+from .feasp import Feasp
 from .feasp import render_template, url_for, redirect, make_response
 
-
+# 有且仅仅提供以下类或函数
 __init__ = [
     Feasp,
-    Response,
     render_template,
     url_for,
     redirect,
