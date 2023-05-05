7d3ea1a316b0fe4e81553bd9d5a8a6f297ea648d
handandkeep <1477792904@qq.com>
2023-05-05 21:57:48 +0800

    added more comments

--- example/app.py
+++ example/app.py
@@ -9,31 +9,48 @@ Or write a view function first, and then implement support in the framework
 """
 
 
+# offer Feasp class to build an app instance,
+# and then you can use this app to make something
+# such as app.response.set_cookie(), app.request.method/cookies, ...
+# app.response offer: status, mimetype, headers, set_cookie()
+# app.request offer: url, method, cookies, ...
 app = Feasp(__name__)
 
 
 @app.route("/string", methods=["GET"])
 def string():
+    # you can return a string,
+    # to verify the app can run
     return "Hello String"
 
 
 @app.route("/dict", methods=["GET"])
 def dict_():
+    # support return dict type to client,
+    # feasp will convert dict to json and satisfy HTTP protocol
     return {"H": "L", "P": "D"}
 
 
 @app.route("/", methods=["GET"])
 def index():
+    # offer the tool func for you,
+    # and use it to render a html file in templates
     return render_template("index.html")
 
 
 @app.route("/image", methods=["GET"])
 def image():
+    # deal the image-file request from html,
+    # and it is built in feasp
+    # for more, css/js are same built-in Feasp,
+    # you just need to define in html
     return render_template("image.html")
 
 
 @app.route("/redirect", methods=["GET"])
 def redirect_():
+    # offer the tool func for you
+    # and use it to redirect a new web page
     return redirect(url_for("index"))
 
 
@@ -76,19 +93,24 @@ def login():
 
 @app.route("/variable/<string:name>", methods=["GET"])
 def show_variable(name):
-    """ The variables must match what define in the template,
-      and input like this: key=value, pass in the variables and values you want to render """
+    # you can define a variable in route path
+    # but the variables must match what define in the template
+    # and input like this: key=value, pass in the variables and values you want to render
     return render_template("variable.html", name=name, love="you")
 
 
 @app.route("/for_list", methods=["GET"])
 def for_list():
+    # you can pass in a list or iterable object
+    # to finish a repeat structure work
     name_list = ["XueLian", "XueXue", "XueFeng"]
     return render_template("for_list.html", name_list=name_list)
 
 
 @app.route("/make_resp", methods=["GET"])
 def make_resp():
+    # offer a tool func for you
+    # and use it to return a custom response
     return make_response(
         "<h1>Hello MakeResponse</h1>", mimetype="text/html", status=202)
 
