b5b712afef5158b1ef67d3ead81fb503de9515ec
handandkeep <1477792904@qq.com>
2023-07-11 21:29:19 +0800

    change format

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -31,7 +31,8 @@ class TestBasic(unittest.TestCase):
             'CONTENT_TYPE': 'text/plain',
             'HTTP_HOST': '127.0.0.1:8000',
             'HTTP_CONNECTION': 'keep-alive',
-            'HTTP_USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.64',
+            'HTTP_USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                               'Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.64',
             'HTTP_SEC_CH_UA_PLATFORM': 'Windows',
             'HTTP_REFERER': '',
             'wsgi.url_scheme': 'http'
@@ -63,21 +64,42 @@ class TestBasic(unittest.TestCase):
             response.headers)
 
     def test_template(self):
-        plain_html = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><link rel="icon" href="/favicon.ico"><link rel="stylesheet" href="/style.css"><title>Title</title></head><body><h1 id="title">Hello World</h1></body><script src="/my_js.js"></script></html>"""
+        plain_html = """ 
+            <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><link rel="icon" href="/favicon.ico">
+            <link rel="stylesheet" href="/style.css"><title>Title</title></head><body><h1 id="title">Hello World</h1>
+            </body><script src="/my_js.js"></script></html> """
         t = FeaspTemplate(plain_html, None)
         self.assertEqual(plain_html, t.render())
 
-        var_html = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><link rel="icon" href="/favicon.ico"><link rel="stylesheet" href="/style.css"><title>Title</title></head><body><h1 id="title">Hello {{name}}</h1></body><script src="/my_js.js"></script></html>"""
-        correct_html = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><link rel="icon" href="/favicon.ico"><link rel="stylesheet" href="/style.css"><title>Title</title></head><body><h1 id="title">Hello XueFeng</h1></body><script src="/my_js.js"></script></html>"""
-        t = FeaspTemplate(var_html, {"name":"XueFeng"})
+        var_html = """
+            <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><link rel="icon" href="/favicon.ico">
+            <link rel="stylesheet" href="/style.css"><title>Title</title></head><body>
+            <h1 id="title">Hello {{name}}</h1></body><script src="/my_js.js"></script></html> """
+
+        correct_html = """
+            <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><link rel="icon" href="/favicon.ico">
+            <link rel="stylesheet" href="/style.css"><title>Title</title></head>
+            <body><h1 id="title">Hello XueFeng</h1></body><script src="/my_js.js"></script></html> """
+        t = FeaspTemplate(var_html, {"name": "XueFeng"})
         self.assertEqual(var_html.replace("{{name}}", "XueFeng"), t.render())
 
-        for_html = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Title</title></head><body><ol><h1>Three: </h1>{% for name in name_list %}<h2>Hello {{ name }}</h2>{% endfor %}</ol></body></html>"""
-        correct_html = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Title</title></head><body><ol><h1>Three: </h1><h2>Hello XueLian</h2><h2>Hello XueXue</h2><h2>Hello XueFeng</h2></ol></body></html>"""
+        for_html = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8">
+            <title>Title</title></head><body><ol><h1>Three: </h1>{% for name in name_list %}
+            <h2>Hello {{ name }}</h2>{% endfor %}</ol></body></html> """
+
+        correct_html = """
+            <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8">
+            <title>Title</title></head><body><ol><h1>Three: </h1><h2>Hello XueLian</h2>
+            <h2>Hello XueXue</h2><h2>Hello XueFeng</h2></ol></body></html> """
         t = FeaspTemplate(for_html, {"name_list": ["XueLian", "XueXue", "XueFeng"]})
         self.assertEqual(correct_html, t.render())
 
-        v_f_html = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Title</title></head><body><ol><h1>{{name}}: </h1>{% for name in name_list %}<h2>Hello {{ name }}</h2>{% endfor %}</ol></body></html>"""
-        correct_html = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Title</title></head><body><ol><h1>Three: </h1><h2>Hello XueLian</h2><h2>Hello XueXue</h2><h2>Hello XueFeng</h2></ol></body></html>"""
-        t = FeaspTemplate(for_html, {"name":"Three", "name_list": ["XueLian", "XueXue", "XueFeng"]})
+        v_f_html = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8">
+        <title>Title</title></head><body><ol><h1>{{name}}: </h1>{% for name in name_list %}
+        <h2>Hello {{ name }}</h2>{% endfor %}</ol></body></html> """
+
+        correct_html = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8">
+        <title>Title</title></head><body><ol><h1>Three: </h1><h2>Hello XueLian</h2>
+        <h2>Hello XueXue</h2><h2>Hello XueFeng</h2></ol></body></html> """
+        t = FeaspTemplate(for_html, {"name": "Three", "name_list": ["XueLian", "XueXue", "XueFeng"]})
         self.assertEqual(correct_html, t.render())
