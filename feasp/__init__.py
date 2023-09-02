a6c852d391ed4aa467884790bd76b895d930f11f
handandkeep <1477792904@qq.com>
2023-09-02 19:49:56 +0800

    update

--- feasp/__init__.py
+++ feasp/__init__.py
@@ -1,9 +1,10 @@
-from .feasp import Feasp
+from .feasp import Feasp, SimpleSqlite
 from .feasp import render_template, url_for, redirect, make_response
 
 
 __all__ = [
     Feasp,
+    SimpleSqlite,
     render_template,
     url_for,
     redirect,
