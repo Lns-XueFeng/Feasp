3ddf9dd46a6e798b007d456411ffda6ef58c6dd2
handandkeep <1477792904@qq.com>
2023-10-19 12:21:50 +0800

    add some tests

--- tests/test_fire_app.py
+++ tests/test_fire_app.py
@@ -9,61 +9,79 @@ import requests
 class TestActualApp(unittest.TestCase):
 
     def test_index(self):
-        req = requests.get("http://127.0.0.1:8000")
-        self.assertIn("Hello World", req.text)
+        res = requests.get("http://127.0.0.1:8000")
+        self.assertIn("Hello World", res.text)
 
     def test_string(self):
-        req = requests.get("http://127.0.0.1:8000/string")
-        self.assertIn("Hello String", req.text)
+        res = requests.get("http://127.0.0.1:8000/string")
+        self.assertIn("Hello String", res.text)
 
     def test_dict_(self):
-        req = requests.get("http://127.0.0.1:8000/dict")
-        self.assertEqual({"H": "L", "P": "D"}, req.json())
+        res = requests.get("http://127.0.0.1:8000/dict")
+        self.assertEqual({"H": "L", "P": "D"}, res.json())
 
     def test_image(self):
-        req = requests.get("http://127.0.0.1:8000/image")
-        self.assertEqual(200, req.status_code)
+        res = requests.get("http://127.0.0.1:8000/image")
+        self.assertEqual(200, res.status_code)
 
     def test_file(self):
-        req = requests.get("http://127.0.0.1:8000/favicon.ico")
-        self.assertEqual(200, req.status_code)
-        req = requests.get("http://127.0.0.1:8000/static/head.jpg")
-        self.assertEqual(200, req.status_code)
-        req = requests.get("http://127.0.0.1:8000/static/my_js.js")
-        self.assertEqual(200, req.status_code)
-        req = requests.get("http://127.0.0.1:8000/static/style.css")
-        self.assertEqual(200, req.status_code)
+        res = requests.get("http://127.0.0.1:8000/favicon.ico")
+        self.assertEqual(200, res.status_code)
+        res = requests.get("http://127.0.0.1:8000/static/head.jpg")
+        self.assertEqual(200, res.status_code)
+        res = requests.get("http://127.0.0.1:8000/static/my_js.js")
+        self.assertEqual(200, res.status_code)
+        res = requests.get("http://127.0.0.1:8000/static/style.css")
+        self.assertEqual(200, res.status_code)
 
     def test_redirect_(self):
-        req = requests.get("http://127.0.0.1:8000/redirect")
-        self.assertIn("Hello World", req.text)
+        res = requests.get("http://127.0.0.1:8000/redirect")
+        self.assertIn("Hello World", res.text)
 
     def test_cookies(self):
         session = requests.session()
-        req = session.get("http://127.0.0.1:8000/show_cookies")
-        self.assertIn("No Cookies", req.text)
-        req = session.get("http://127.0.0.1:8000/set_cookies")
-        self.assertIn("Set Cookies", req.text)
-        req = session.get("http://127.0.0.1:8000/show_cookies")
-        self.assertIn("Name=XueFeng;Hobby=WriteCode", req.text)
+        res = session.get("http://127.0.0.1:8000/show_cookies")
+        self.assertIn("No Cookies", res.text)
+        res = session.get("http://127.0.0.1:8000/set_cookies")
+        self.assertIn("Set Cookies", res.text)
+        res = session.get("http://127.0.0.1:8000/show_cookies")
+        self.assertIn("Name=XueFeng;Hobby=WriteCode", res.text)
 
     def test_login(self):
-        req = requests.get("http://127.0.0.1:8000/login")
-        self.assertEqual(200, req.status_code)
-        form = {"username": "XueFeng", "password": "123456789"}
-        req = requests.post("http://127.0.0.1:8000/login", data=form)
-        self.assertIn("Your login correctly !", req.text)
+        res = requests.get("http://127.0.0.1:8000/login")
+        self.assertEqual(200, res.status_code)
+        form = {"username": "", "password": ""}
+        res = requests.post("http://127.0.0.1:8000/login", data=form)
+        self.assertIn("Please Input user and password !", res.text)
+        form = {"username": "Lns-XueFeng", "password": "123456789"}
+        res = requests.post("http://127.0.0.1:8000/login", data=form)
+        self.assertIn("Your login message correctly !", res.text)
+        form = {"username": "Lns-XueFeng", "password": "111111111"}
+        res = requests.post("http://127.0.0.1:8000/login", data=form)
+        self.assertIn("Your login message incorrectly !", res.text)
 
     def test_variable(self):
-        req = requests.get("http://127.0.0.1:8000/variable/XueFeng")
-        self.assertIn("Hello XueFeng, I love you", req.text)
+        res = requests.get("http://127.0.0.1:8000/variable/XueFeng")
+        self.assertIn("Hello XueFeng, I love you", res.text)
 
     def test_for_list(self):
-        req = requests.get("http://127.0.0.1:8000/for_list")
-        self.assertIn("Hello XueLian", req.text)
-        self.assertIn("Hello XueXue", req.text)
-        self.assertIn("Hello XueFeng", req.text)
+        res = requests.get("http://127.0.0.1:8000/for_list")
+        self.assertIn("Hello XueLian", res.text)
+        self.assertIn("Hello XueXue", res.text)
+        self.assertIn("Hello XueFeng", res.text)
 
     def test_make_resp(self):
-        req = requests.get("http://127.0.0.1:8000/make_resp")
-        self.assertIn("Hello MakeResponse", req.text)
+        res = requests.get("http://127.0.0.1:8000/make_resp")
+        self.assertIn("Hello MakeResponse", res.text)
+
+    def test_see_my_func(self):
+        res = requests.get("http://127.0.0.1:8000/see_my_func")
+        self.assertIn("see_funcs", res.text)
+
+    def test_if_control(self):
+        res = requests.get("http://127.0.0.1:8000/if_control")
+        self.assertIn("Hello Lns-XueFeng", res.text)
+
+    def test_if_and_for(self):
+        res = requests.get("http://127.0.0.1:8000/if_and_for")
+        self.assertIn("Hello XueFeng", res.text)
